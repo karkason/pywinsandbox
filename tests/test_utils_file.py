057f32cc800adc3242e6ed2031de3b1e6f7f464b
Yiftach Karkason <ykarkason@gmail.com>
2020-03-23 22:41:37 +0200

    Fix race in test_wait_for_file_creation between 2 calls to time.time()

--- tests/test_utils_file.py
+++ tests/test_utils_file.py
@@ -1,13 +1,10 @@
 from winsandbox.utils.file import wait_for_file_creation
 
-import os
 import tempfile
 
 
 def test_wait_for_file_creation():
-    temp_file_path = tempfile.mktemp()
-    with open(temp_file_path, "w") as f:
-        assert wait_for_file_creation(f.name, timeout=0, check_interval=0)
+    with tempfile.NamedTemporaryFile() as f:
+        assert wait_for_file_creation(f.name, timeout=0)
 
-    os.remove(temp_file_path)
-    assert not wait_for_file_creation(f.name, timeout=0)
+    assert not wait_for_file_creation(f.name, timeout=0, check_interval=0)
--- winsandbox/utils/file.py
+++ winsandbox/utils/file.py
@@ -7,6 +7,9 @@ def wait_for_file_creation(filename, timeout=25, check_interval=1):
     Return true if filename exists, if not keep checking once every check_interval seconds for time_limit seconds.
     """
 
+    if timeout < 1:
+        timeout = 1
+
     now = time.time()
     timeout_time = now + timeout
 
